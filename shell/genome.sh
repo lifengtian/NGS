31e7c2ca98afb21f488829899cf0ff5a75ccab9e
Lifeng Tian <lifeng4209@gmail.com>
2011-11-29 11:28:47 -0500

    Update shell/genome.sh

--- shell/genome.sh
+++ shell/genome.sh
@@ -61,22 +61,25 @@ platform="-dP illumina "
 # 24gb is the default size in this script
 
 
-#### paths
-#temp=/scratch/local
+#### 
+# GATK
+#export JAVA_BIN=$JAVA_HOME/bin/java
+#export GATK=/scr2/caglab/gatk
+#export GATK_JAR=$GATK/GenomeAnalysisTK.jar
+#export JAVA_GATK="$JAVA_BIN -Djava.io.tmpdir=. -Xmx6g -jar $GATK_JAR"
+#export HG19=$GATK/hg19/hg19.fa
+#export PICARD=$GATK/picard
+#export SAMTOOLS=$GATK/bin/samtools
+#export BWA=$GATK/bin/bwa
+#export FASTQC=$GATK/FastQC
+
 temp=$p
-#cagapps=/work/1a/cagapps
-cagapps=/scr2/caglab/
-gatk=$cagapps/gatk
-samtools=$gatk/samtools/samtools
-R=$gatk/R
+
+R=$GATK/R
 Rscript=$R/2.7.0/bin/Rscript
-git=$gatk/git
-#beagle="$gatk/jdk/jdk1.6.0_22/bin/java -Djava.io.tmpdir=$temp -Xmx24g -jar $gatk/beagle/beagle.jar"
+
 beagle="/usr/java/latest/bin/java -Djava.io.tmpdir=$temp -Xmx24g -jar $gatk/beagle/beagle.jar"
-#java="$gatk/jdk/jdk1.6.0_22/bin/java -Djava.io.tmpdir=$temp -Xmx6g"
-java="/usr/java/latest/bin/java -Djava.io.tmpdir=$temp -Xmx6g"
-javagatk="$java -jar $git/dist/GenomeAnalysisTK.jar -DBQ 3"  # what a bug!
-picard=$gatk/picard
+
 
 
 chr=(chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY)
@@ -91,14 +94,13 @@ dbsnp=$gatk/hg18/dbsnp_132.hg18.vcf
 kg_indels=1000G_indels_for_realignment.hg18.vcf
 
 elif [ "$ref_version" = "b37" ]; then
-#ref=$gatk/b37/human_g1k_v37.fasta
-#ref=/abings/2nd1a/references/hg19_solid/hg19_validated.fa
-ref=$gatk/hg19.fa
-hapmap=$gatk/b37/hapmap_3.3.b37.sites.vcf
-kg=$gatk/b37/1000G_omni2.5.b37.sites.vcf
-dbsnp=$gatk/b37/dbsnp_132.b37.vcf
-kg_indels=$gatk/b37/1000G_indels_for_realignment.b37.vcf
-Mills_Devine=$gatk/b37/Mills_Devine_2hit.indels.b37.sites.vcf
+
+ref=$HG19
+hapmap=$GATK/b37/hapmap_3.3.b37.sites.vcf
+kg=$GATK/b37/1000G_omni2.5.b37.sites.vcf
+dbsnp=$GATK/b37/dbsnp_132.b37.vcf
+kg_indels=$GATK/b37/1000G_indels_for_realignment.b37.vcf
+Mills_Devine=$GATK/b37/Mills_Devine_2hit.indels.b37.sites.vcf
 
 else
 	echo "$ref_version has to be hg18 or b37"
@@ -131,12 +133,12 @@ count=0                                    # temp variable to count jobs submitt
 
 #### check required files
 check_list=($ref $hapmap $kg $dbsnp \
-$samtools \
-$git/dist/GenomeAnalysisTK.jar \
-$gatk/beagle/beagle.jar \
-$picard/MarkDuplicates.jar \
-$picard/CollectAlignmentSummaryMetrics.jar \
-$picard/MeanQualityByCycle.jar \
+$SAMTOOLS \
+$GATK_JAR \
+$GATK/beagle/beagle.jar \
+$PICARD/MarkDuplicates.jar \
+$PICARD/CollectAlignmentSummaryMetrics.jar \
+$PICARD/MeanQualityByCycle.jar \
 $Rscript \
 $Mills_Devine)
 
@@ -155,7 +157,9 @@ if [ "$bye" = "yes" ]; then
 	exit
 fi        
 
-hold_jid=100000000
+#### DON'T forget the ','!
+hold_jid=100000000,
+
 
 #### Step 1. Cleanup BAM files
 
@@ -166,7 +170,7 @@ date > $p/step1.log
 for i in ${bamlist[*]}; do
 
     echo "
-	###############################################################################
+    ###############################################################################
     #cleanup bams (index, rmdup, index) for $i
     ###############################################################################
 
@@ -181,7 +185,7 @@ for i in ${bamlist[*]}; do
         -R $ref \
         -o $p/$i.dedup.intervals \
         -I $p/$i.dedup.bam \
-		-T RealignerTargetCreator
+	-T RealignerTargetCreator
 
     # 2. Indel Realigner
     $javagatk  \
@@ -189,7 +193,7 @@ for i in ${bamlist[*]}; do
         -R $ref \
         -targetIntervals $p/$i.dedup.intervals \
         -o $p/$i.dedup.indelrealigner.bam \
-		-T IndelRealigner
+	-T IndelRealigner
         
 
     # Base quality score recalibration
@@ -247,19 +251,19 @@ if [ ! -f $p/step2.log ]; then
 	date >> $p/step2.log
 
 	for i in ${bamlist[*]};do
-    	bams=$bams" -I $p/"$i.dedup.indelrealigner.recal.bam
+    		bams=$bams" -I $p/"$i.dedup.indelrealigner.recal.bam
 	done
 
 	count=0
 
-	#chr=(chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY)
 	for c in ${chr[*]}; do
-    	echo "$javagatk -T UnifiedGenotyper -glm BOTH  -R $ref $bams -o $vcf.$c.vcf $opt  -L $c  -A AlleleBalance -A DepthOfCoverage -A FisherStrand
-    	" > $p/gatkscripts/$run.step2.$c.sh
-    	job=`qsub -hold_jid $hold_jid -e $p/log/$run.step2.$c.log -o $p/log/$run.step2.$c.log $p/gatkscripts/$run.step2.$c.sh`
-    	ug_jobs[$count]=`echo $job | awk '{print $3}'`
-    	echo UnifiedGenotyper job for $c: ${ug_jobs[$count]} submitted
-    	let count=count+1
+    		echo "$javagatk -T UnifiedGenotyper -glm BOTH  -R $ref $bams -o $vcf.$c.vcf $opt  -L $c  -A AlleleBalance -A DepthOfCoverage -A FisherStrand
+    		" > $p/gatkscripts/$run.step2.$c.sh
+    	
+		job=`qsub -hold_jid $hold_jid -e $p/log/$run.step2.$c.log -o $p/log/$run.step2.$c.log $p/gatkscripts/$run.step2.$c.sh`
+    		ug_jobs[$count]=`echo $job | awk '{print $3}'`
+    		echo UnifiedGenotyper job for $c: ${ug_jobs[$count]} submitted
+    		let count=count+1
 	done
 
 
@@ -283,179 +287,179 @@ for i in ${bamlist[*]}; do
     $samtools flagstat $p/$i.dedup.indelrealigner.recal.bam > $p/$i.dedup.indelrealigner.recal.flagstat
     " > $p/gatkscripts/$run.$i.step3.dedup.summary.sh
 	
-   # job=`qsub -hold_jid $hold_jid -o $p/log/$run.$i.step3.dedup.log $p/gatkscripts/$run.$i.step3.dedup.summary.sh`
-   # tempjob=`echo $job | awk '{print $3}'`
-   # echo [1 SummaryMetrics: $tempjob ] waiting for UnifiedGenotyper: $hold_jid
+    job=`qsub -hold_jid $hold_jid -o $p/log/$run.$i.step3.dedup.log $p/gatkscripts/$run.$i.step3.dedup.summary.sh`
+    tempjob=`echo $job | awk '{print $3}'`
+    echo [1 SummaryMetrics: $tempjob ] waiting for UnifiedGenotyper: $hold_jid
                        
     echo "
 	$javagatk -T DepthOfCoverage -R $ref -I $p/$i.dedup.indelrealigner.recal.bam -o $p/$i.recal.doc -omitBaseOutput -ct 1 -ct 2 -ct 3 -ct 4 -ct 5 -ct 6 -ct 7 -ct 8
 	" > $p/gatkscripts/$run.$i.step3.recal.doc.sh
-   # job=`qsub -hold_jid $hold_jid  -o $p/log/$run.$i.step3.recal.doc.log $p/gatkscripts/$run.$i.step3.recal.doc.sh`
-   # tempjob=`echo $job | awk '{print $3}'`
-   # echo [1 DepthOfCoverage: $tempjob ] waiting for UnifiedGenotyper: $hold_jid
+   job=`qsub -hold_jid $hold_jid  -o $p/log/$run.$i.step3.recal.doc.log $p/gatkscripts/$run.$i.step3.recal.doc.sh`
+   tempjob=`echo $job | awk '{print $3}'`
+   echo [1 DepthOfCoverage: $tempjob ] waiting for UnifiedGenotyper: $hold_jid
 	
-    ## summarize original BAM statistics
+   summarize original BAM statistics
     echo "
        $java -jar $picard/CollectAlignmentSummaryMetrics.jar I=$p/$i.bam O=$p/$i.picard.summary R=$ref VALIDATION_STRINGENCY=SILENT
        $java -jar $picard/MeanQualityByCycle.jar I=$p/$i.bam O=$p/$i.meanquality CHART=$p/$i.meanquality.pdf VALIDATION_STRINGENCY=SILENT
        $samtools flagstat $p/$i.bam > $p/$i.flagstat
        $javagatk -T DepthOfCoverage -R $ref -I $p/$i.bam -o $p/$i.doc -omitBaseOutput -ct 1 -ct 2 -ct 3 -ct 4 -ct 5 -ct 6 -ct 7 -ct 8
        " > $p/gatkscripts/$run.$i.step3.doc.sh
-    #job=`qsub -hold_jid $hold_jid -o $p/log/$run.$i.step3.doc.log $p/gatkscripts/$run.$i.step3.doc.sh`
-    #tempjob=`echo $job | awk '{print $3}'`
-    #echo [1 Summary of Original BAM: $tempjob ] waiting for UnifiedGenotyper: $hold_jid
+    job=`qsub -hold_jid $hold_jid -o $p/log/$run.$i.step3.doc.log $p/gatkscripts/$run.$i.step3.doc.sh`
+    tempjob=`echo $job | awk '{print $3}'`
+    echo [1 Summary of Original BAM: $tempjob ] waiting for UnifiedGenotyper: $hold_jid
 
 done
 
 
 if [ ! -f $p/step4.log ]; then
-echo step4 started > $p/step4.log
-date >> $p/step4.log
+	echo step4 started > $p/step4.log
+	date >> $p/step4.log
 
 ############################### Step 4 combine Variants, Annotation, VQSR recalibration ###############################################
-for c in ${chr[*]}; do
-    vcf_chr=$vcf_chr" -V $vcf.$c.vcf"
-done
+	for c in ${chr[*]}; do
+    		vcf_chr=$vcf_chr" -V $vcf.$c.vcf"
+	done
 
 
-echo "
-    #### combine VCFs
-    #$javagatk -T CombineVariants $vcf_chr -o $vcf -R $ref
+	echo "
+    		#### combine VCFs
+    		$javagatk -T CombineVariants $vcf_chr -o $vcf -R $ref
 
-    #### produce SNP and INDEL VCFs 
-    $javagatk -T SelectVariants \
-        -R $ref \
-        -V $vcf \
-		-o $snp  \
-		-env   \
-		-selectType SNP  
+    		#### produce SNP and INDEL VCFs 
+    		$javagatk -T SelectVariants \
+        		-R $ref \
+        		-V $vcf \
+			-o $snp  \
+			-env   \
+			-selectType SNP  
 
-    $javagatk -T SelectVariants \
-		-R $ref \
-        -V $vcf \
-		-o $indel  \
-		-env  \
-		-selectType INDEL 
+    		$javagatk -T SelectVariants \
+			-R $ref \
+        		-V $vcf \
+			-o $indel  \
+			-env  \
+			-selectType INDEL 
 
-" > $p/gatkscripts/$run.step4.sh
-
-
-### for whole exome, we have several choices here
-### Here in this pipeline, we do GATK
-### followed by
-### 1. Normal QUAL and QD filtering
-### 2. VQSR
+	" > $p/gatkscripts/$run.step4.sh
 
 
-## Oct 27, 2011 based on Best Practice 3
-## QD < 2.0", "MQ < 40.0", "FS > 60.0", "HaplotypeScore > 13.0", "MQRankSum < -12.5", "ReadPosRankSum < -8.0".
-echo "
-	$javagatk -T VariantFiltration \
-		-V $snp \
-		-o $snp.temp.vcf \
-		--clusterSize 3 \
-		--clusterWindowSize 10 \
-		--filterExpression \"QD < 2.0 || MQ < 40.0 || FS > 60.0 || HaplotypeScore > 13.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0\" \
-		--filterName GATKSNPStandard \
-		-R $ref \
-
-	$javagatk -T SelectVariants \
-		-R $ref \
-		-V $snp.temp.vcf \
-		-o $snp.gatkstandard.vcf  \
-		-env \
-		-ef
+	### for whole exome, we have several choices here
+	### Here in this pipeline, we do GATK
+	### followed by
+	### 1. Normal QUAL and QD filtering
+	### 2. VQSR
 
 
-" >> $p/gatkscripts/$run.step4.sh
+	## Oct 27, 2011 based on Best Practice 3
+	## QD < 2.0", "MQ < 40.0", "FS > 60.0", "HaplotypeScore > 13.0", "MQRankSum < -12.5", "ReadPosRankSum < -8.0".
+	echo "
+		$javagatk -T VariantFiltration \
+			-V $snp \
+			-o $snp.temp.vcf \
+			--clusterSize 3 \
+			--clusterWindowSize 10 \
+			--filterExpression \"QD < 2.0 || MQ < 40.0 || FS > 60.0 || HaplotypeScore > 13.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0\" \
+			--filterName GATKSNPStandard \
+			-R $ref \
 
-### Indel
-### DATA_TYPE_SPECIFIC_FILTERS should be "QD < 2.0", "ReadPosRankSum < -20.0", "InbreedingCoeff < -0.8", "FS > 200.0".
-echo "
+		$javagatk -T SelectVariants \
+			-R $ref \
+			-V $snp.temp.vcf \
+			-o $snp.gatkstandard.vcf  \
+			-env \
+			-ef
+
+
+	" >> $p/gatkscripts/$run.step4.sh
+
+	### Indel
+	### DATA_TYPE_SPECIFIC_FILTERS should be "QD < 2.0", "ReadPosRankSum < -20.0", "InbreedingCoeff < -0.8", "FS > 200.0".
+	echo "
 	    $javagatk -T VariantFiltration \
-        -V $indel \
-        -o $indel.temp.vcf \
-        --clusterSize 3 \
-        --clusterWindowSize 10 \
-        --filterExpression \"QD < 2.0 ||  ReadPosRankSum < -20.0 || FS > 200.0 || InbreedingCoeff < -0.8  \" \
-        --filterName GATKINDELStandard \
-        -R $ref \
+        	-V $indel \
+        	-o $indel.temp.vcf \
+        	--clusterSize 3 \
+        	--clusterWindowSize 10 \
+        	--filterExpression \"QD < 2.0 ||  ReadPosRankSum < -20.0 || FS > 200.0 || InbreedingCoeff < -0.8  \" \
+        	--filterName GATKINDELStandard \
+        	-R $ref \
 
 	
-    $javagatk -T SelectVariants \
-        -R $ref \
-        -V $indel.temp.vcf \
-        -o $indel.gatkstandard.vcf  \
-        -env \
-        -ef
+    	$javagatk -T SelectVariants \
+        	-R $ref \
+        	-V $indel.temp.vcf \
+        	-o $indel.gatkstandard.vcf  \
+        	-env \
+        	-ef
 
 
-" >> $p/gatkscripts/$run.step4.sh
+	" >> $p/gatkscripts/$run.step4.sh
 
-if [ "$VQSR" = "yes" ]; then
-echo "
-    ###########################################################################################
-    # VQSR
-    # IN: variants as input, hapmap, 1kg, dbsnp as training
-    # OUT: cluster file
-    ###########################################################################################
+	if [ "$VQSR" = "yes" ]; then
+		echo "
+    		###########################################################################################
+    		# VQSR
+    		# IN: variants as input, hapmap, 1kg, dbsnp as training
+    		# OUT: cluster file
+    		###########################################################################################
 
-   $javagatk \
-   -T VariantRecalibrator \
-   -R $ref \
-   -input $snp \
-   -resource:hapmap,known=false,training=true,truth=true,prior=15.0 $hapmap \
-   -resource:omni,known=false,training=true,truth=false,prior=12.0 $kg \
-   -resource:dbsnp,known=true,training=false,truth=false,prior=8.0 $dbsnp \
-   -an QD -an HaplotypeScore -an MQRankSum -an ReadPosRankSum -an MQ -an FS -an DP \
-   -mode SNP \
-   -recalFile $snp_recal \
-   -tranchesFile $snp_tranches \
-   -Rscript /usr/bin/Rscript \
-   -resources $git/public/R \
-   -rscriptFile $p/rscript.snp.r \
+   		$javagatk \
+   			-T VariantRecalibrator \
+   			-R $ref \
+   			-input $snp \
+   			-resource:hapmap,known=false,training=true,truth=true,prior=15.0 $hapmap \
+   			-resource:omni,known=false,training=true,truth=false,prior=12.0 $kg \
+   			-resource:dbsnp,known=true,training=false,truth=false,prior=8.0 $dbsnp \
+   			-an QD -an HaplotypeScore -an MQRankSum -an ReadPosRankSum -an MQ -an FS -an DP \
+   			-mode SNP \
+   			-recalFile $snp_recal \
+   			-tranchesFile $snp_tranches \
+   			-Rscript $Rscript \
+   			-resources $git/public/R \
+   			-rscriptFile $p/rscript.snp.r \
 
-   $javagatk \
-   -T ApplyRecalibration \
-   -R $ref \
-   -input $snp \
-   --ts_filter_level 99.0 \
-   -tranchesFile $snp_tranches \
-   -recalFile $snp_recal \
-   -o $snp.vqsr.vcf
+   		$javagatk \
+   			-T ApplyRecalibration \
+   			-R $ref \
+   			-input $snp \
+   			--ts_filter_level 99.0 \
+   			-tranchesFile $snp_tranches \
+   			-recalFile $snp_recal \
+   			-o $snp.vqsr.vcf
 
-    #indel -an InbreedingCoeff is not included
-   $javagatk \
-   -T VariantRecalibrator \
-   -R $ref \
-   -input $indel \
-   -resource:mills,VCF,known=true,training=true,truth=true,prior=12.0 $Mills_Devine \
-   -an QD -an FS -an HaplotypeScore -an ReadPosRankSum  \
-   -mode INDEL \
-   -recalFile $indel_recal \
-   -tranchesFile $indel_tranches \
-   -Rscript /usr/bin/Rscript \
-   -resources $git/public/R \
-   -rscriptFile $p/rscript.indel.r \
+    		#indel -an InbreedingCoeff is not included
+   		$javagatk \
+   			-T VariantRecalibrator \
+   			-R $ref \
+   			-input $indel \
+   			-resource:mills,VCF,known=true,training=true,truth=true,prior=12.0 $Mills_Devine \
+   			-an QD -an FS -an HaplotypeScore -an ReadPosRankSum  \
+   			-mode INDEL \
+   			-recalFile $indel_recal \
+   			-tranchesFile $indel_tranches \
+   			-Rscript $Rscript \
+   			-resources $git/public/R \
+   			-rscriptFile $p/rscript.indel.r \
 
-   $javagatk \
-   -T ApplyRecalibration \
-   -R $ref \
-   -input $indel \
-   --ts_filter_level 99.0 \
-   -tranchesFile $indel_tranches \
-   -recalFile $indel_recal \
-   -o $indel.vqsr.vcf
+   		$javagatk \
+   			-T ApplyRecalibration \
+   			-R $ref \
+   			-input $indel \
+   			--ts_filter_level 99.0 \
+   			-tranchesFile $indel_tranches \
+   			-recalFile $indel_recal \
+   			-o $indel.vqsr.vcf
 
-" >> $p/gatkscripts/$run.step4.sh
-fi
+		" >> $p/gatkscripts/$run.step4.sh
+	fi
 
-job=`qsub -hold_jid $hold_jid -e $p/log/$run.step4.log -o $p/log/$run.step4.log $p/gatkscripts/$run.step4.sh`
-hold_jid=`echo $job | awk '{print $3}'`
-echo [3 SelectVariants: $filter_job ] waiting for UnifiedGenotyper: $hold_jid 
+	job=`qsub -hold_jid $hold_jid -e $p/log/$run.step4.log -o $p/log/$run.step4.log $p/gatkscripts/$run.step4.sh`
+	hold_jid=`echo $job | awk '{print $3}'`
+	echo [3 SelectVariants: $filter_job ] waiting for UnifiedGenotyper: $hold_jid 
 
-echo step4 ended >> $p/step4.log
-date >> $p/step4.log
+	echo step4 ended >> $p/step4.log
+	date >> $p/step4.log
 fi #end_of_step4
 
 
