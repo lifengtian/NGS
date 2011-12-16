2f5663bdca50a1b291aed562c550f0222793cfdd
Lifeng Tian <lifeng4209@gmail.com>
2011-12-16 15:44:58 -0500

    fix perl warnings

--- perl/intersectCSV_by_gene.pl
+++ perl/intersectCSV_by_gene.pl
@@ -13,16 +13,16 @@ my $count3;
 
 my $in1    = $ARGV[0];
 my $in2    = $ARGV[1];
-my $first  = $ARGV[2];
-my $all    = $ARGV[3];
-my $second = $ARGV[4];
+my $first_fn  = $ARGV[2];
+my $all_fn    = $ARGV[3];
+my $second_fn = $ARGV[4];
 
-open F1, $in1;
-open F2, $in2;
+open (my $F1, $in1);
+open (my $F2, $in2);
 
-open FIRST,  ">$first";
-open ALL,    ">$all";
-open SECOND, ">$second";
+open (my $FIRST,  ">$first_fn");
+open (my $ALL,    ">$all_fn");
+open (my $SECOND, ">$second_fn");
 
 my %h1;
 my %h;
@@ -30,26 +30,26 @@ my %h2;
 
 my $csv = Text::CSV->new();
 
-<F1>;
-<F2>;
+<$F1>;
+<$F2>;
 
-while (<F1>) {
+while (<$F1>) {
     chomp;
     if ( $csv->parse($_) ) {
         my @a = $csv->fields();
         $h1{ $a[1] }{ join( "\t", @a[ 15 .. 19 ] ) } = $_;
     }
 }
-close F1;
+close $F1;
 
-while (<F2>) {
+while (<$F2>) {
     chomp;
     if ( $csv->parse($_) ) {
         my @a = $csv->fields();
         $h2{ $a[1] }{ join( "\t", @a[ 15 .. 19 ] ) } = $_;
     }
 }
-close F2;
+close $F2;
 
 #find keys shared or unique
 my @k1 = keys %h1;
@@ -63,26 +63,26 @@ my @l_only2 = $lc->get_Ronly();
 
 foreach (@l_inter) {
     foreach my $k ( keys %{ $h1{$_} } ) {
-        print ALL $h1{$_}->{$k}, "\n";
+        print $ALL $h1{$_}->{$k}, "\n";
     }
     foreach my $k ( keys %{ $h2{$_} } ) {
-        print ALL $h2{$_}->{$k}, "\n";
+        print $ALL $h2{$_}->{$k}, "\n";
     }
 }
 
 foreach (@l_only2) {
     foreach my $k ( keys %{ $h2{$_} } ) {
-        print SECOND $h2{$_}->{$k}, "\n";
+        print $SECOND $h2{$_}->{$k}, "\n";
     }
 }
 
 foreach (@l_only1) {
     foreach my $k ( keys %{ $h1{$_} } ) {
-        print FIRST $h1{$_}->{$k}, "\n";
+        print $FIRST $h1{$_}->{$k}, "\n";
     }
 }
 
-close ALL;
-close FIRST;
-close SECOND;
+close $ALL;
+close $FIRST;
+close $SECOND;
 
