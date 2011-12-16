fef1ef7168a4dc6664f4366ebc3da23c87afa9ff
Lifeng Tian <lifeng4209@gmail.com>
2011-12-16 15:32:16 -0500

    add intersectCSV_by_gene.pl

--- perl/intersectCSV_by_gene.pl
+++ perl/intersectCSV_by_gene.pl
@@ -1,28 +1,28 @@
+use strict;
+use warnings;
 use Text::CSV;
+use List::Compare;
 
 =head1 note
 
-gene may have multiple lines. 
 =cut
 
 my $count1;
 my $count2;
 my $count3;
 
+my $in1    = $ARGV[0];
+my $in2    = $ARGV[1];
+my $first  = $ARGV[2];
+my $all    = $ARGV[3];
+my $second = $ARGV[4];
 
+open F1, $in1;
+open F2, $in2;
 
-my $in1=$ARGV[0];
-my $in2=$ARGV[1];
-my $first=$ARGV[2];
-my $all=$ARGV[3];
-my $second=$ARGV[4];
-
-open F1,$in1;
-open F2,$in2;
-
-open FIRST,">$first";
-open ALL ,">$all";
-open SECOND,">$second";
+open FIRST,  ">$first";
+open ALL,    ">$all";
+open SECOND, ">$second";
 
 my %h1;
 my %h;
@@ -33,57 +33,56 @@ my $csv = Text::CSV->new();
 <F1>;
 <F2>;
 
-while(<F1>){
-	chomp;
-	if ( $csv->parse($_) ) {
-        my @a=$csv->fields();	
-		$h1{$a[1]}{join("\t",@a[15..19])}=$_;
-		$h{$a[1]}=1;
-		$count1++;
-	}
+while (<F1>) {
+    chomp;
+    if ( $csv->parse($_) ) {
+        my @a = $csv->fields();
+        $h1{ $a[1] }{ join( "\t", @a[ 15 .. 19 ] ) } = $_;
+    }
 }
 close F1;
 
-while(<F2>){
-	chomp;
-	if ( $csv->parse($_) ) {
-        my @a=$csv->fields(); 
-		$h2{$a[1]}{join("\t",@a[15..19])}=$_;
-		if ( $h{$a[1]} == 1 ) { $h{$a[1]} = 3;$count3++ } else {
-			$h{$a[1]} = 2 ; $count2++
-		}
-	}
+while (<F2>) {
+    chomp;
+    if ( $csv->parse($_) ) {
+        my @a = $csv->fields();
+        $h2{ $a[1] }{ join( "\t", @a[ 15 .. 19 ] ) } = $_;
+    }
 }
 close F2;
 
-print join("\t",$count1,$count2,$count3),"\n";
+#find keys shared or unique
+my @k1 = keys %h1;
+my @k2 = keys %h2;
 
-foreach (keys %h){
-     if ( $h{$_} == 3 ) {
-		
-		foreach my $k ( keys %{$h1{$_}} ) {
-			print ALL $h1{$_}->{$k},"\n"; 
-		}
-		foreach my $k ( keys %{$h2{$_}} ) {
-                        print ALL $h2{$_}->{$k},"\n";
-                }
-	} elsif ( $h{$_} == 2 ) {
-		foreach my $k ( keys %{$h2{$_}} ) {
-                        print SECOND $h2{$_}->{$k},"\n";
-                }
-	} elsif ($h{$_} == 1) {
-		foreach my $k ( keys %{$h1{$_}} ) {
-                        print FIRST $h1{$_}->{$k},"\n";
-                }
-	} else {
-		#print $h1{$_},"\n";
-		#print $h2{$_},"\n";
-		die "$h{$_} error";
-	}
+my $lc = List::Compare->new( \@k1, \@k2 );
+
+my @l_inter = $lc->get_intersection();
+my @l_only1 = $lc->get_Lonly();
+my @l_only2 = $lc->get_Ronly();
+
+foreach (@l_inter) {
+    foreach my $k ( keys %{ $h1{$_} } ) {
+        print ALL $h1{$_}->{$k}, "\n";
+    }
+    foreach my $k ( keys %{ $h2{$_} } ) {
+        print ALL $h2{$_}->{$k}, "\n";
+    }
+}
+
+foreach (@l_only2) {
+    foreach my $k ( keys %{ $h2{$_} } ) {
+        print SECOND $h2{$_}->{$k}, "\n";
+    }
+}
+
+foreach (@l_only1) {
+    foreach my $k ( keys %{ $h1{$_} } ) {
+        print FIRST $h1{$_}->{$k}, "\n";
+    }
 }
 
 close ALL;
 close FIRST;
 close SECOND;
-
 
