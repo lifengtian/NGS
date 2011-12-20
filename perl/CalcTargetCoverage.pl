f123332757b90c35509cfc0772668fc2a0733024
Lifeng Tian <lifeng4209@gmail.com>
2011-12-19 21:48:10 -0500

    add hiseq_target_coverage

---
+++ perl/CalcTargetCoverage.pl
@@ -0,0 +1,127 @@
+
+=head1 note
+## net.sf.picard.metrics.StringHeader
+# net.sf.picard.analysis.directed.CalculateHsMetrics BAIT_INTERVALS=../../AMD/results/baits.picard TARGET_INTERVALS=../../AMD/results/baits.picard INPUT=DS103_CAGATC_L001.sorted.bam OUTPUT=DS103_CAGATC_L001.sorted.bam.coverage VALIDATION_STRINGENCY=SILENT    METRIC_ACCUMULATION_LEVEL=[ALL_READS] TMP_DIR=/tmp/lifeng2 VERBOSITY=INFO QUIET=false COMPRESSION_LEVEL=5 MAX_RECORDS_IN_RAM=500000 CREATE_INDEX=false CREATE_MD5_FILE=false
+## net.sf.picard.metrics.StringHeader
+# Started on: Sat Oct 29 10:47:21 EDT 2011
+
+## METRICS CLASS	net.sf.picard.analysis.directed.HsMetrics
+SAMPLE	LIBRARY	READ_GROUP	BAIT_SET	GENOME_SIZE	BAIT_TERRITORY	TARGET_TERRITORY	BAIT_DESIGN_EFFICIENCY	TOTAL_READS	PF_READS	PF_UNIQUE_READS	PCT_PF_READS	PCT_PF_UQ_READS	PF_UQ_READS_ALIGNED	PCT_PF_UQ_READS_ALIGNED	PF_UQ_BASES_ALIGNED	ON_BAIT_BASES	NEAR_BAIT_BASES	OFF_BAIT_BASES	ON_TARGET_BASES	PCT_SELECTED_BASES	PCT_OFF_BAIT	ON_BAIT_VS_SELECTEDMEAN_BAIT_COVERAGE	MEAN_TARGET_COVERAGE	PCT_USABLE_BASES_ON_BAIT	PCT_USABLE_BASES_ON_TARGET	FOLD_ENRICHMENT	ZERO_CVG_TARGETS_PCT	FOLD_80_BASE_PENALTY	PCT_TARGET_BASES_2X	PCT_TARGET_BASES_10X	PCT_TARGET_BASES_20X	PCT_TARGET_BASES_30X	HS_LIBRARY_SIZE	HS_PENALTY_10X	HS_PENALTY_20X	HS_PENALTY_30X	AT_DROPOUT	GC_DROPOUT
+			baits	3137161264	1343846	1343846	1	7738802	7738802	7738802	1	1	6956673	0.898934	699877964	203242146	34186499	462449319	203242146	0.339243	0.660757	0.856014	151.239164	181.735888	0.260027	0.260027	677.920541	0.013615	3.365479	0.803834	0.779288	0.7524	0.727413		0	0	0	0	0
+
+0	SAMPLE
+1	LIBRARY
+2	READ_GROUP
+3	BAIT_SET
+4	GENOME_SIZE
+5	BAIT_TERRITORY
+6	TARGET_TERRITORY
+7	BAIT_DESIGN_EFFICIENCY
+8	TOTAL_READS
+9	PF_READS
+10	PF_UNIQUE_READS
+11	PCT_PF_READS
+12	PCT_PF_UQ_READS
+13	PF_UQ_READS_ALIGNED
+14	PCT_PF_UQ_READS_ALIGNED
+15	PF_UQ_BASES_ALIGNED
+16	ON_BAIT_BASES
+17	NEAR_BAIT_BASES
+18	OFF_BAIT_BASES
+19	ON_TARGET_BASES
+20	PCT_SELECTED_BASES
+21	PCT_OFF_BAIT
+22	ON_BAIT_VS_SELECTED
+23	MEAN_BAIT_COVERAGE
+24	MEAN_TARGET_COVERAGE
+25	PCT_USABLE_BASES_ON_BAIT
+26	PCT_USABLE_BASES_ON_TARGET
+27	FOLD_ENRICHMENT
+28	ZERO_CVG_TARGETS_PCT
+29	FOLD_80_BASE_PENALTY
+30	PCT_TARGET_BASES_2X
+31	PCT_TARGET_BASES_10X
+32	PCT_TARGET_BASES_20X
+33	PCT_TARGET_BASES_30X
+34	HS_LIBRARY_SIZE
+35	HS_PENALTY_10X
+36	HS_PENALTY_20X
+37	HS_PENALTY_30X
+38	AT_DROPOUT
+39	GC_DROPOUT
+
+=cut
+
+my $header_printed = 0;
+my $paste_file_names;
+
+foreach my $cov (@ARGV) {
+
+    open F,  $cov . '.target_coverage' || die "Error open $cov";
+    open F2, $cov . '.flagstat'        || die "Error open $cov";
+
+    open O, ">" . $cov . '.out';
+
+    $paste_file_names .= "$cov.out ";
+    my $line;
+
+    #skip headers
+    while ( $line =~ /^#/ || $line =~ /^$/ ) {
+        $line = <F>;
+    }
+
+    my @header = split /\s+/, $line;
+    foreach ( 0 .. $#header ) {
+
+        #	 print $_,"\t",$header[$_],"\n";
+    }
+
+## process flagstat
+    <F2>;
+    my $line2 = <F2>;
+    my $line3 = <F2>;
+    my ( $dup,    @t )  = split /\s+/, $line2;
+    my ( $mapped, @t2 ) = split /\s+/, $line3;
+
+    $line = <F>;
+    my @result = split /\s+/, $line;
+    $cov =~ /(.*?).dedup/;
+    $result[0] = $1;
+    foreach ( 2, 4, 6, 8, 15, 17, 19, 24, 28, 31, 32 ) {
+        if ( !$header_printed ) {
+            print O $header[$_], "\t";
+        }
+        if ( $header[$_] =~ /^PCT/ || $header[$_] =~ /PCT$/ ) {
+            print O sprintf( "%.2f", $result[ $_ - 2 ] * 100 ), "\n";
+        }
+        else {
+            if ( $result[ $_ - 2 ] =~ /\d\.\d/ ) {
+                print O sprintf( "%.2f", $result[ $_ - 2 ] ), "\n";
+            }
+            else {
+                print O $result[ $_ - 2 ], "\n";
+            }
+        }
+    }
+    if ( !$header_printed ) {
+        print O "Duplicate_reads\t", $dup,    "\n";
+        print O "Mapped_reads\t",    $mapped, "\n";
+        print O "Duplicate_reads\/Mapped_reads\t",
+          sprintf( "%.2f", $dup / $mapped );
+
+    }
+    else {
+        print O $dup,    "\n";
+        print O $mapped, "\n";
+        print O sprintf( "%.2f", $dup / $mapped );
+
+    }
+
+    close F;
+    close O;
+    close F2;
+
+    $header_printed = 1;
+}
+`paste $paste_file_names > target.coverage`;
+
---
+++ shell/hiseq_target_coverage.sh
@@ -0,0 +1,132 @@
+# calculate target coverage 
+
+source setup.sh
+
+Target=$GATK/bed/SureSelect50mbclean
+Target_bed=$TARGET.bed
+
+###### FUNCTION    check_files ######
+# check the locations of required files
+#
+function check_files {
+
+check_list=( $GENOMECOVERAGE \
+             $SAMTOOLS \
+             $BWA \
+             $HG19 \
+             $JAVA_BIN \
+             $FASTQC/fastqc \ 
+             $PICARD/MarkDuplicates.jar \
+	     $Target_bed \
+	     $HISEQ/NGS/perl/CalcTargetCoverage.pl \	
+)
+
+bye='no'
+for i in ${check_list[*]}; do
+if [ ! -f $i ]; then
+    echo $i not exist
+        bye='yes'
+fi
+done
+
+echo bye is $bye
+
+if [ "$bye" = "yes" ]; then
+        echo See you next time
+        exit
+fi
+
+}
+
+
+
+
+######### FUNCTION bwa ###########
+# run BWA
+##################################
+function calc_coverage {
+p=$1
+sm=$2
+fid=$3
+lane=$4
+index=$5
+
+if [ ! -f $p/$sm-$lane-$index.dedup.bam ]; then
+  echo $p/$sm-$lane-$index.dedup.bam not exist!
+  exit
+fi
+
+if [ ! -f $p/$sm-$lane-$index.sorted.bam ]; then
+	echo $p/$sm-$lane-$index.sorted.bam not exist!
+	exit
+fi
+
+echo "
+$JAVA_BIN -jar $PICARD/CalculateHsMetrics.jar I=$p/$sm-$lane-$index.dedup.bam O=$p/$sm-$lane-$index.dedup.bam.target_coverage BI=$cov/SureSelect50mbclean.picard TI=$cov/SureSelect50mbclean.picard TMP_DIR=/scratch/local
+
+$SAMTOOLS depth -b $Target_bed $p/$sm-$lane-$index.sorted.bam | perl $GENOMECOVERAGE > $p/$sm-$lane-$index.sorted.bam.target_depth
+$SAMTOOLS depth -b $Target_bed $p/$sm-$lane-$index.dedup.bam | perl $GENOMECOVERAGE > $p/$sm-$lane-$index.dedup.bam.target_depth
+$HISEQ/NGS/perl/CalcTargetCoverage.pl $p/$sm-$lane-$index.dedup.bam 
+" > $p/scripts/$sm-$lane-$index.target_coverage.sh
+
+#job=`qsub $queue -V -e $p/log/$sm-$lane-$index.target_coverage.err.log -o $p/log/$sm-$lane-$index.target_coverage.log $p/scripts/$sm-$lane-$index.target_coverage.sh`
+
+}
+
+######## FUNCTION feed_bwa #########
+# find all fastq.gz pairs 
+# create Aligned folder
+# prepare calls for bwa 
+####################################
+
+function feed_bwa {
+flow_cell_id=$1
+sample_sheet=$HISEQ_ANALYSIS/SampleSheet/$flow_cell_id.csv
+fastq=$HISEQ_ANALYSIS/FASTQ/$flow_cell_id
+bam=$HISEQ_ANALYSIS/BAM/$flow_cell_id
+
+
+
+#check files
+check_files
+
+if [ ! -d $bam ]; then
+    mkdir -p $bam
+else
+#
+	echo "$bam exist. Exit"
+	exit
+fi
+
+# parse sample_sheet
+#FCID,Lane,SampleID,SampleRef,Index,Description,Control,Recipe,Operator,SampleProject
+#C0995ACXX,1,lane1,Unknown,,'DefaultSample',N,,,C0995ACXX
+count=0
+cat $sample_sheet | while read LINE
+do
+let count++
+if [ $count -gt 1 ]; then
+echo "$count $LINE"
+FCID=`echo $LINE | cut -f1 -d','`
+Lane=`echo $LINE | cut -f2 -d','`
+SampleID=`echo $LINE | cut -f3 -d','`
+Index=`echo $LINE | cut -f5 -d','`
+SampleProject=`echo $LINE | cut -f10 -d','`
+
+
+if [ ! $Index ]; then
+Index="NoIndex"
+fi
+
+bwa $bam/Project_$SampleProject/Sample_$SampleID $SampleID $FCID $Lane $Index
+#sleep 10
+fi # end_of_count -gt 1
+
+done # end_of_while read sample_sheet
+
+}
+
+
+#feed_bwa FlowCellID
+feed_bwa    $1
+
