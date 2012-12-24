3d06cfa2edf0c44aefed4ae09a9857608a71bc4c
Lifeng Tian <tianl@f0def10dc2a1.chop.edu>
2012-12-24 18:42:34 -0500

    add UnifiedGenotyper.snp.scala UnifiedGenotyper.indel.scala

--- QUEUE/UnifiedGenotyper.scala
+++ QUEUE/UnifiedGenotyper.scala
@@ -6,6 +6,8 @@ import org.broadinstitute.sting.queue.extensions.gatk._
 import org.broadinstitute.sting.commandline.Hidden
 import org.broadinstitute.sting.gatk.phonehome._
 
+import org.broadinstitute.sting.gatk.walkers.genotyper.GenotypeLikelihoodsCalculationModel
+
   
 
 /**
@@ -29,11 +31,18 @@ class Genotyper extends QScript {
   @Input(doc="An optional file with a list of intervals to proccess.", shortName="L", required=false)
   var intervals: File = _
 
-  @Argument(doc="A optional list of filter names.", shortName="filter", required=false)
-  var filterNames: List[String] = Nil // Nil is an empty List, versus null which means a non-existent List.
+  @Argument(doc="A optional list of filter names.", shortName="snpfilter", required=false)
+  var snpfilterNames: List[String] =  List("LowQualityDepth","MappingQuality","StrandBias","HaplotypeScoreHigh","MQRankSumLow","ReadPosRankSumLow" )
 
-  @Argument(doc="An optional list of filter expressions.", shortName="filterExpression", required=false)
-  var filterExpressions: List[String] = Nil
+  @Argument(doc="An optional list of filter expressions.", shortName="snpfilterExpression", required=false)
+  var snpfilterExpressions: List[String] = List ("QD < 2.0", "MQ < 40.0", "FS > 60.0", "HaplotypeScore > 13.0", "MQRankSum < -12.5", "ReadPosRankSum < -8.0")
+
+  @Argument(doc="A optional list of filter names.", shortName="indelfilter", required=false)
+  var indelfilterNames: List[String] =  List("LowQualityDepth","StrandBias","ReadPosRankSumLow","InbreedingCoeffLow" )
+
+  @Argument(doc="An optional list of filter expressions.", shortName="indelfilterExpression", required=false)
+  var indelfilterExpressions: List[String] = List ("QD < 2.0", "FS > 200.0",  "ReadPosRankSum < -20.0", "InbreedingCoeff < -0.8")
+
 
   // This trait allows us set the variables below in one place,
   // and then reuse this trait on each CommandLineGATK function below.
@@ -50,32 +59,88 @@ class Genotyper extends QScript {
   @Argument(doc="How many ways to scatter/gather", fullName="scatter_gather", shortName="sg", required=false)
   var nContigs: Int = -1
 
-  def script() {
-    // Create the four functions that we may run depending on options.
+  /*  Functions  */
+  def call_genotypes_indel(bams: Seq[File])  {
     val genotyper = new UnifiedGenotyper with UnifiedGenotyperArguments
     val variantFilter = new VariantFiltration with UnifiedGenotyperArguments
     val evalUnfiltered = new VariantEval with UnifiedGenotyperArguments
     val evalFiltered = new VariantEval with UnifiedGenotyperArguments
 
-    genotyper.scatterCount = nContigs 
-    genotyper.input_file = qscript.bamFile
-    genotyper.out = "out.UnifiedGenotyper.raw.vcf"
+    genotyper.input_file = bams
+    genotyper.out = "unifiedgenotyper.indel.vcf"
+
+    genotyper.genotype_likelihoods_model = GenotypeLikelihoodsCalculationModel.Model.INDEL
 
     evalUnfiltered.eval :+= genotyper.out
     evalUnfiltered.out = swapExt(genotyper.out, "vcf", "eval")
 
     variantFilter.variant = genotyper.out
     variantFilter.out = swapExt(genotyper.out, "vcf", "filtered.vcf")
-    variantFilter.filterName = filterNames
-    variantFilter.filterExpression = filterExpressions
+    variantFilter.filterName = indelfilterNames
+    variantFilter.filterExpression = indelfilterExpressions
 
     evalFiltered.eval :+= variantFilter.out
     evalFiltered.out = swapExt(variantFilter.out, "vcf", "eval")
 
     add(genotyper, evalUnfiltered)
     // Only add variant filtration to the pipeline if filters were passed in
-    if (filterNames.size > 0)
+    if (indelfilterNames.size > 0)
       add(variantFilter, evalFiltered)
+}
+
+def call_genotypes_snp(bams: Seq[File])  {
+    val genotyper = new UnifiedGenotyper with UnifiedGenotyperArguments
+    val variantFilter = new VariantFiltration with UnifiedGenotyperArguments
+    val evalUnfiltered = new VariantEval with UnifiedGenotyperArguments
+    val evalFiltered = new VariantEval with UnifiedGenotyperArguments
+
+    genotyper.input_file = bams
+    genotyper.out = "unifiedgenotyper.snp.vcf"
+
+    genotyper.genotype_likelihoods_model = GenotypeLikelihoodsCalculationModel.Model.SNP
+
+    evalUnfiltered.eval :+= genotyper.out
+    evalUnfiltered.out = swapExt(genotyper.out, "vcf", "eval")
+
+    variantFilter.variant = genotyper.out
+    variantFilter.out = swapExt(genotyper.out, "vcf", "filtered.vcf")
+    variantFilter.filterName = snpfilterNames
+    variantFilter.filterExpression = snpfilterExpressions
+
+    evalFiltered.eval :+= variantFilter.out
+    evalFiltered.out = swapExt(variantFilter.out, "vcf", "eval")
+
+    add(genotyper, evalUnfiltered)
+    // Only add variant filtration to the pipeline if filters were passed in
+    if (snpfilterNames.size > 0)
+      add(variantFilter, evalFiltered)
+}
+
+
+  def annotate_snpEff(inVcf: File) {
+        val eff = new SnpEff
+        eff.config = new File("/mnt/isilon/cag/ngs/hiseq/respublica/pipeline/gatk/snpEff_2_0_5/snpEff.config")
+        eff.genomeVersion = "GRCh37.64"
+
+	eff.inVcf = inVcf
+	var snpEffout: File  = swapExt(eff.inVcf, "vcf", "snpEff.out")
+        eff.outVcf = swapExt(eff.inVcf, "vcf", "snpEff.vcf")
+        
+        eff.javaClasspath = List("/mnt/isilon/cag/ngs/hiseq/respublica/pipeline/gatk/snpEff_2_0_5/")
+        eff.jarFile = "/mnt/isilon/cag/ngs/hiseq/respublica/pipeline/gatk/snpEff_2_0_5/snpEff.jar"
+    
+        add(eff)
+        add(varannotator("out.unfiltered.filtered.vcf","out.snpEff.vcf","out.final.vcf"))
+}
+
+  def script() {
+
+	call_genotypes_snp(bamFile)
+	call_genotypes_indel(bamFile)
+	annotate_snpEff("unifiedgenotyper.indel.filtered.vcf")
+	annotate_snpEff("unifiedgenotyper.snp.filtered.vcf")
+
+
   }
 }
 
