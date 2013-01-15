66d58d4673d9e68a806577044608e05e1d033cc7
Lifeng Tian <lifeng4209@gmail.com>
2013-01-15 14:40:31 -0500

    add tclib.c watchjobs.c

--- c/char.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-  char str_a[20];
-  int i;
-
-  return 1;
-}
--- c/test.c
+++ c/test.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char ** argv){
    if (argc<2) {
-      fprintf(stderr, "usage: example <path-to-job>\n");
+      fprintf(stderr, "usage: test <path-to-jobs>\n");
       return 1;
    } else {
 	printf("%s\t%s\tsize:%d\t%d\n", argv[0],argv[1],sizeof(char),sizeof(char *));
